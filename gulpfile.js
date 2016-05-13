e38c689967d5d090c78065e49d11031e1a7469d4
Manolo Carrasco <manolo@vaadin.com>
2016-05-13 07:47:11 +0200

    Adding generated bundles

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,45 @@
 var gulp = require('gulp');
 var vulcanize = require('gulp-vulcanize');
 var htmlmin = require('gulp-htmlmin');
+var minifyInline = require('gulp-minify-inline');
 var rename = require("gulp-rename");
 var insert = require('gulp-insert');
-var minifyInline = require('gulp-minify-inline');
-var removeHtmlComments = require('gulp-remove-html-comments');
 var bundle = 'polymer-bundle';
 
+var jsname = bundle + '.js';
+var htmlname = bundle + '.html';
+var minname = bundle + '.min.html';
+
 gulp.task('bootstrap', function() {
-  var jsname = bundle + '.js';
   return gulp.src('bower_components/webcomponentsjs/webcomponents-lite.min.js')
           .pipe(rename(jsname))
           .pipe(insert.append(
-            'var path = document.querySelector(\'script[src$="' + jsname + '"]\').src.replace(/' + jsname + '.js$/,\'\');' +
-            'document.write(\'<link rel="import" href="\' + path + bundle + \'.min.html">\');'
+            'var path = document.querySelector(\'script[src$="' + jsname + '"]\').src.replace(/' + jsname + '$/,\'\');' +
+            'document.write(\'<link rel="import" href="\' + path + \'' + minname + '">\');'
           ))
           .pipe(gulp.dest('.'));
 });
 
 gulp.task('vulcanize', function () {
-	return gulp.src( bundle + '.html')
+	return gulp.src(htmlname)
 		.pipe(vulcanize({
 			abspath: '',
 			stripExcludes: false,
       inlineScripts: true,
       inlineCss: true,
-      stripComments: true,
       // TODO: adding 'bower_components/iron-doc-viewer/iron-doc-viewer.html'
       // does not works and prevents including certain things like polymer.html.
       excludes: [
       ]
 		}))
-    .pipe(removeHtmlComments())
-    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true,
+      removeAttributeQuotes: true,
+      removeRedundantAttributes: true
+    }))
     .pipe(minifyInline())
-    .pipe(rename(bundle + ".min.html"))
+    .pipe(rename(minname))
 		.pipe(gulp.dest('.'));
 });
 
